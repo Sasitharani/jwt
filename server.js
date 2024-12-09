9b5a5b85a50a2a7f24cafd58b0c5158b14a38534
Sasi <sasitharani@gmail.com>
2024-12-09 13:20:00 +0530

    19

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ console.log('hpass',password);
             res.status(500).send('Signup failed. Please try again.');
             return;
         }
-        res.status(201).send({ message: 'User registered successfully!' });
+        res.status(201).send({ message: 'User registered successfully!',results });
     });
 });
 
--- src/Pass.jsx
+++ src/Pass.jsx
@@ -17,7 +17,7 @@ const Pass = () => {
                 password
             });
             const { hashedPassword } = response.data;
-            console.log(hashedPassword)
+            console.log(response.data)
             setHashedPassword(hashedPassword);
             setMessage('Password hashed successfully!');
         } catch (error) {
