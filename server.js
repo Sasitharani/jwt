50cd8759340f0f5539ffe3998e53767ae58bcdfd
sasi <sasitharani@gmail.com>
2024-12-23 15:33:41 +0530

    #10 affectedRows

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.post('/reset-password', (req, res) => {
     db.query(query, [newPassword, email], (err, results) => {
         console.log('Results password change:', results);
         console.log('Results password affectedRows:', results.affectedRows);
-        if (err) {
+        if (results.affectedRows==0) {
             console.error('Error updating password:', err);
             res.status(500).send('Error resetting password. Please try again.');
             return;
