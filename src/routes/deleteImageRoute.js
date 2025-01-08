052f69b42fe345f065d9e3d91a741f842c94ecce
sasi <sasitharani@gmail.com>
2025-01-08 11:13:25 +0530

    #7

--- src/routes/deleteImageRoute.js
+++ src/routes/deleteImageRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteImage } from './controllers/deleteImageController';
+import { deleteImage } from '../controllers/deleteImageController';
 
 const router = express.Router();
 
