12614c3f12594ea42205820c6fbd6c49d787d604
sasi <sasitharani@gmail.com>
2025-01-09 17:31:55 +0530

    #25

--- src/controllers/updateVotesController.js
+++ src/controllers/updateVotesController.js
@@ -10,41 +10,36 @@ const updateVotes = async (req, res) => {
 
     try {
         const query = `SELECT MaxLikes, LikesUsed FROM ${tableName}`;
-        db.query(query, (err, result) => {
+        db.query(query, (err, results) => {
             if (err) {
                 console.error('Error fetching data:', err);
                 res.status(500).send('Server error');
                 return;
             }
 
-            console.log('Query result:', result);
-            if (result) {
-                console.log('Result recordset:', result.recordset);
-                if (result.recordset.length > 0) {
-                    console.log('MaxLikes and LikesUsed fetched:', result.recordset[0]);
-                    const { MaxLikes, LikesUsed } = result.recordset[0];
+            console.log('Query results:', results);
 
-                    if (MaxLikes > 0 && MaxLikes != LikesUsed) {
-                        const updateQuery = `UPDATE ${tableName} SET LikesUsed = LikesUsed + 1`;
-                        db.query(updateQuery, (err, result) => {
-                            if (err) {
-                                console.error('Error updating data:', err);
-                                res.status(500).send('Server error');
-                                return;
-                            }
-                            console.log('Vote updated successfully');
-                            res.status(200).send('Vote updated successfully');
-                        });
-                    } else {
-                        console.log('All likes used, no likes left');
-                        res.status(400).send('All likes used, no likes left');
-                    }
+            if (results && results.length > 0) {
+                console.log('MaxLikes and LikesUsed fetched:', results[0]);
+                const { MaxLikes, LikesUsed } = results[0];
+
+                if (MaxLikes > 0 && MaxLikes != LikesUsed) {
+                    const updateQuery = `UPDATE ${tableName} SET LikesUsed = LikesUsed + 1`;
+                    db.query(updateQuery, (err, updateResults) => {
+                        if (err) {
+                            console.error('Error updating data:', err);
+                            res.status(500).send('Server error');
+                            return;
+                        }
+                        console.log('Vote updated successfully');
+                        res.status(200).send('Vote updated successfully');
+                    });
                 } else {
-                    console.log('No data found');
-                    res.status(404).send('No data found');
+                    console.log('All likes used, no likes left');
+                    res.status(400).send('All likes used, no likes left');
                 }
             } else {
-                console.log('Result recordset is undefined');
+                console.log('No data found');
                 res.status(404).send('No data found');
             }
         });
