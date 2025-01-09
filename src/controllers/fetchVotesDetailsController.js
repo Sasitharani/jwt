015a478988229bf98d724d96a3df58f92baceeae
sasi <sasitharani@gmail.com>
2025-01-09 17:35:49 +0530

    #26

--- src/controllers/fetchVotesDetailsController.js
+++ src/controllers/fetchVotesDetailsController.js
@@ -3,22 +3,22 @@ import db from '../../db.js';
 const checkAndInsertDefaultValues = async (tableName, username, email) => {
     try {
         const query = `SELECT * FROM ${tableName}`;
-        db.query(query, (err, result) => {
+        db.query(query, (err, results) => {
             if (err) {
                 console.error('Error fetching data:', err);
                 throw new Error('Error checking or inserting default values');
             }
 
-            console.log('Result of SELECT query:', result);
+            console.log('Result of SELECT query:', results);
 
-            if (result && result.recordset && result.recordset.length === 0) {
+            if (results && results.length === 0) {
                 const insertQuery = `
                     INSERT INTO ${tableName} (username, email, MaxLikes)
                     VALUES (?, ?, 10)
                 `;
                 if (username && email) {
                     const values = [username, email];
-                    db.query(insertQuery, values, (err, result) => {
+                    db.query(insertQuery, values, (err, insertResults) => {
                         if (err) {
                             console.error('Error inserting data:', err);
                             throw new Error('Error checking or inserting default values');
@@ -37,21 +37,21 @@ const fetchVotesDetails = async (req, res) => {
     const today = new Date().toISOString().split('T')[0].replace(/-/g, '');
     const tableName = `todaysDateLikes_${today}`;
     const { username, email } = req.body;
-//     const username = useSelector(state => state.user.username);
+
     try {
         await checkAndInsertDefaultValues(tableName, username, email);
         const query = `SELECT * FROM ${tableName}`;
-        db.query(query, (err, result) => {
+        db.query(query, (err, results) => {
             if (err) {
                 console.error('Error fetching data:', err);
                 res.status(500).send('Server error');
                 return;
             }
 
-            console.log('Result of SELECT query:', result);
+            console.log('Result of SELECT query:', results);
 
-            if (result.recordset && result.recordset.length > 0) {
-                res.status(200).json(result.recordset);
+            if (results && results.length > 0) {
+                res.status(200).json(results);
             } else {
                 res.status(404).send('No data found');
             }
