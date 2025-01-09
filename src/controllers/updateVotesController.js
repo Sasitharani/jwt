c14b35a6041d31feec5338ba6b5554622e7f56d6
sasi <sasitharani@gmail.com>
2025-01-09 17:25:37 +0530

    #24

--- src/controllers/updateVotesController.js
+++ src/controllers/updateVotesController.js
@@ -18,31 +18,35 @@ const updateVotes = async (req, res) => {
             }
 
             console.log('Query result:', result);
-            console.log('Result recordset:', result.recordset);
-            if (result.recordset && result.recordset.length > 0) {
-                console.log('MaxLikes and LikesUsed fetched:', result.recordset[0]);
-                const { MaxLikes, LikesUsed } = result.recordset[0];
+            if (result) {
+                console.log('Result recordset:', result.recordset);
+                if (result.recordset.length > 0) {
+                    console.log('MaxLikes and LikesUsed fetched:', result.recordset[0]);
+                    const { MaxLikes, LikesUsed } = result.recordset[0];
 
-                if (MaxLikes > 0 && MaxLikes != LikesUsed) {
-                    const updateQuery = `UPDATE ${tableName} SET LikesUsed = LikesUsed + 1`;
-                    db.query(updateQuery, (err, result) => {
-                        if (err) {
-                            console.error('Error updating data:', err);
-                            res.status(500).send('Server error');
-                            return;
-                        }
-                        console.log('Vote updated successfully');
-                        res.status(200).send('Vote updated successfully');
-                    });
+                    if (MaxLikes > 0 && MaxLikes != LikesUsed) {
+                        const updateQuery = `UPDATE ${tableName} SET LikesUsed = LikesUsed + 1`;
+                        db.query(updateQuery, (err, result) => {
+                            if (err) {
+                                console.error('Error updating data:', err);
+                                res.status(500).send('Server error');
+                                return;
+                            }
+                            console.log('Vote updated successfully');
+                            res.status(200).send('Vote updated successfully');
+                        });
+                    } else {
+                        console.log('All likes used, no likes left');
+                        res.status(400).send('All likes used, no likes left');
+                    }
                 } else {
-                    console.log('All likes used, no likes left');
-                    res.status(400).send('All likes used, no likes left');
+                    console.log('No data found');
+                    res.status(404).send('No data found');
                 }
             } else {
-                console.log('No data found');
+                console.log('Result recordset is undefined');
                 res.status(404).send('No data found');
             }
-    
         });
     } catch (err) {
         console.error('Error in updateVotes:', err);
