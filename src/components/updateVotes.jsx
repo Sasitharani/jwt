e5f36e5eb3c10622f81ef0b136a30a64a764f1c0
sasi <sasitharani@gmail.com>
2025-01-09 17:39:22 +0530

    #27

--- src/components/updateVotes.jsx
+++ src/components/updateVotes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import axios from 'axios';
-import swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 const UpdateVotesButton = () => {
     const handleClick = async () => {
         try {
             const response = await axios.post('https://jwt-rj8s.onrender.com/api/updateVotes');
-            swal('Success', response.data, 'success');
+            Swal.fire('Success', response.data, 'success');
         } catch (error) {
-            swal('Error', error.response.data, 'error');
+            Swal.fire('Error', error.response.data, 'error');
         }
     };
 
