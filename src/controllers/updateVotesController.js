84361218c1017c14f6a7f27e1da975f43c6face1
sasi <sasitharani@gmail.com>
2025-01-10 10:02:35 +0530

    #6

--- src/controllers/updateVotesController.js
+++ src/controllers/updateVotesController.js
@@ -44,6 +44,36 @@ const updateVotes = async (req, res) => {
                 });
             } else {
                 console.log("Enter the else loop in updateVotesController");
+                const selectQuery = `SELECT MaxLikes, LikesUsed FROM ${tableName} WHERE email = ?`;
+                db.query(selectQuery, [email], (err, selectResults) => {
+                    if (err) {
+                        console.error('Error selecting data:', err);
+                        res.status(500).send('Server error');
+                        return;
+                    }
+
+                    if (selectResults.length > 0) {
+                        const { MaxLikes, LikesUsed } = selectResults[0];
+                        if (MaxLikes === LikesUsed) {
+                            console.log('All likes used, no likes left');
+                            res.status(400).send('All likes used, no likes left');
+                        } else {
+                            const updateQuery = `UPDATE ${tableName} SET LikesUsed = LikesUsed + 1 WHERE email = ?`;
+                            db.query(updateQuery, [email], (err, updateResults) => {
+                                if (err) {
+                                    console.error('Error updating data:', err);
+                                    res.status(500).send('Server error');
+                                    return;
+                                }
+                                console.log('Vote updated successfully');
+                                res.status(200).send('Vote updated successfully');
+                            });
+                        }
+                    } else {
+                        console.log('No data found for the given email');
+                        res.status(404).send('No data found for the given email');
+                    }
+                });
             }
         });
     } catch (err) {
