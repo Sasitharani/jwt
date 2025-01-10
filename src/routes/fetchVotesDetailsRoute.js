7a11726c1bff9bbe6a2f553cd0c84529a80df008
sasi <sasitharani@gmail.com>
2025-01-10 11:07:21 +0530

    #12

--- src/routes/fetchVotesDetailsRoute.js
+++ src/routes/fetchVotesDetailsRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import { fetchVotesDetails } from '../controllers/fetchVotesDetailsController.js';
 
-router.get('/fetchVotesDetails', fetchVotesDetails);
+router.post('/fetchVotesDetails', fetchVotesDetails);
 
 export default router;
 router.use((req, res, next) => {
