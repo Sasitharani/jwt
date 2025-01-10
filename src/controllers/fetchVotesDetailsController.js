c1ce44ecfd84a7f1a3aa18353b4acba08a50d731
sasi <sasitharani@gmail.com>
2025-01-10 10:07:24 +0530

    #7

--- src/controllers/fetchVotesDetailsController.js
+++ src/controllers/fetchVotesDetailsController.js
@@ -1,46 +1,10 @@
 import db from '../../db.js';
 
-const checkAndInsertDefaultValues = async (tableName, username, email) => {
-    try {
-        const query = `SELECT * FROM ${tableName}`;
-        db.query(query, (err, results) => {
-            if (err) {
-                console.error('Error fetching data:', err);
-                throw new Error('Error checking or inserting default values');
-            }
-
-            console.log('results.length of SELECT query:', results.length);
-
-            if (results.length === 0) {
-                console.log("Insert new Data")
-                const insertQuery = `
-                    INSERT INTO ${tableName} (username, email, MaxLikes)
-                    VALUES (?, ?, 10)
-                `;
-                if (username && email) {
-                    const values = [username, email];
-                    db.query(insertQuery, values, (err, insertResults) => {
-                        if (err) {
-                            console.error('Error inserting data:', err);
-                            throw new Error('Error checking or inserting default values');
-                        }
-                    });
-                }
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        throw new Error('Error checking or inserting default values');
-    }
-};
-
 const fetchVotesDetails = async (req, res) => {
     const today = new Date().toISOString().split('T')[0].replace(/-/g, '');
     const tableName = `todaysDateLikes_${today}`;
-    const { username, email } = req.body;
 
     try {
-        await checkAndInsertDefaultValues(tableName, username, email);
         const query = `SELECT * FROM ${tableName}`;
         db.query(query, (err, results) => {
             if (err) {
